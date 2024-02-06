e4ba5954bc665ac99614b6fbb9ea4cbb52df909c
Trash-panda330 <uetak330@gmail.com>
2024-02-06 23:39:32 +0900

    初コミット

--- src/main/java/projava/Main.java
+++ src/main/java/projava/Main.java
@@ -2,6 +2,6 @@ package projava;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello ShinJava");
     }
 }
