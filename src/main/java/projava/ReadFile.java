d197a11faa6df26840b38cd2fa097a54f6f51da8
Trash-panda330 <uetak330@gmail.com>
2024-09-07 18:14:04 +0900

    コードに解説を記載

--- src/main/java/projava/ReadFile.java
+++ src/main/java/projava/ReadFile.java
@@ -1,17 +1,23 @@
 package projava;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
+import java.io.IOException; // ファイル操作で発生する可能性のある例外を扱うためにインポート
+import java.nio.file.Files; // ファイル操作を行うクラス
+import java.nio.file.NoSuchFileException; // ファイルが存在しない場合に投げられる例外
+import java.nio.file.Path; // ファイルパスを扱うクラス
 
 public class ReadFile {
     public static void main(String[] args) throws IOException {
         try {
+            // "text.txt" というファイルのパスオブジェクトを作成
             var p = Path.of("text.txt");
+
+            // 指定されたファイルの内容を読み込み、文字列として取得
             String s = Files.readString(p);
+
+            // 読み込んだファイルの内容をコンソールに出力
             System.out.println(s);
         } catch (NoSuchFileException e) {
+            // ファイルが見つからなかった場合に例外をキャッチし、エラーメッセージを出力
             System.out.println("ファイルが見つかりません：" + e.getFile());
         }
     }
