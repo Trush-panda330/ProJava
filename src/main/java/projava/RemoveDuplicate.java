1f9e45d6fb927df38dee09ea69f35c773b864bab
Trash-panda330 <uetak330@gmail.com>
2024-09-08 12:48:50 +0900

    "abcccbaabcc"を"abcbabc"のように重複を除くプログラム
    新しいStringBuilderを作りfor文の中でひとつ前の文字と同じかどうか調べ同じなら処理をせず(continueで処理を抜け)違う場合appendで追加する

--- src/main/java/projava/RemoveDuplicate.java
+++ src/main/java/projava/RemoveDuplicate.java
@@ -2,17 +2,21 @@ package projava;
 
 public class RemoveDuplicate {
     public static void main(String[] args) {
-        var data = "abcccbaabcc" ;
+        var data = "abcccbaabcc";
 
         var builder = new StringBuilder();
+        //builderに重複を除いた新しい文字列を作る処理を行う
         for (int i = 0; i < data.length(); i++) {
-            char ch = data.charAt(i);
-            if (i > 0 && ch == data.charAt(i - 1)) {
+            char c = data.charAt(i);
+            //iが0の時は前に文字がないため比較しようがないため処理はしない
+            //またiの時のcがひとつ前(i - 1)の文字と同じ時も処理をしない
+            if(i > 0 && c == data.charAt(i - 1))
                 continue;
+            builder.append(c);
             }
-            builder.append(ch);
-        }
-        var result = builder.toString();
+        /*全ての文字の処理が終わったらtoString()で
+        StringBuilderオブジェクトに構築された文字列を取得する*/
+        String result = builder.toString();
         System.out.println(data);
         System.out.println(result);
     }
