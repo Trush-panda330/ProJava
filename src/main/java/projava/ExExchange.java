f78420ff1b3a76de5ffe2fdd3f5b0f619201c056
Trash-panda330 <uetak330@gmail.com>
2024-09-08 17:52:39 +0900

    文字列の奇数番目の文字と偶数番目の文字を入れ替えるプログラム

--- src/main/java/projava/ExExchange.java
+++ src/main/java/projava/ExExchange.java
@@ -2,17 +2,23 @@ package projava;
 
 public class ExExchange {
     public static void main(String[] args) {
-        var data = "abcde"; //文字列の宣言
+        var data = "abcde"; // 入力データとしての文字列を宣言
+        // StringBuilder を生成（文字列の構築用）
+        var builder = new StringBuilder();
 
-        var builder = new StringBuilder(); //ストリングビルダーメソッドを生成
+        // 奇数番目の文字だけ処理すればよいので更新分はi += 2
         for (int i = 0; i < data.length(); i += 2) {
+            // 次の文字が文字列の長さより短い時(=最後の文字でない場合）次の文字を追加
             if (i + 1 < data.length()) {
-                builder.append(data.charAt(i + 1));
+                builder.append(data.charAt(i + 1)); // 次の文字を追加
             }
+            // 現在のインデックスの文字を追加
             builder.append(data.charAt(i));
         }
+
+        // StringBuilder から最終的な文字列を取得
         var result = builder.toString();
-        System.out.println(data);
-        System.out.println(result);
+        System.out.println(data); // 元の文字列
+        System.out.println(result); // 変換後
     }
 }
